c6c9fad007f9db50a33c694f10421d3f993fc5f8
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2021-08-25 13:13:35 +0500

    add active class in nav bar but not and efficient way

--- php_files/header.php
+++ php_files/header.php
@@ -18,12 +18,11 @@
         <a href="/learn_PHP_MVC" class="logo">Welcome to PHP CRUD</a>
         <div class="menu-toggle"></div>
         <nav>
-            <ul>
-                <!-- <li><a href="/learn_PHP_MVC" class="active">Home</a></li> -->
-                <li><a href="/learn_PHP_MVC">Home</a></li>
-                <li><a href="/learn_PHP_MVC/addProduct/index">Add Products</a></li>
-                <li><a href="/learn_PHP_MVC/update/index">Update Products</a></li>
-                <li><a href="/learn_PHP_MVC/delete/index">Delete Products</a></li>
+            <ul id="nav-list">
+                <li><a id="home-link" class="" href="/learn_PHP_MVC">Home</a></li>
+                <li><a id="add-link" class="" href="/learn_PHP_MVC/addProduct/index">Add Products</a></li>
+                <li><a id="update-link" class="" href="/learn_PHP_MVC/update/index">Update Products</a></li>
+                <li><a id="delete-link" class="" href="/learn_PHP_MVC/delete/index">Delete Products</a></li>
             </ul>
         </nav>
         <div class="clearfix"></div>
@@ -31,16 +30,35 @@
 
     <script type="text/javascript" src="/learn_PHP_MVC/js_files/jquery-3.6.0.js"></script>
     <script type="text/javascript" src="/learn_PHP_MVC/js_files/initial-load-table.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.menu-toggle').click(function() {
+                $('.menu-toggle').toggleClass('active');
+                $('nav').toggleClass('active');
+                // $('nav ul li a').removeClass('active')   
+            })
+        })
+
+        $(document).ready(function() {
+            function getEventTarget(e) {
+                e = e || window.event;
+                return e.target || e.srcElement;
+            }
+
+            var url = window.location.href;
+            if (url == "http://localhost/learn_PHP_MVC/") {
+                document.querySelector("#home-link").classList.add("active");
+            } else if (url == "http://localhost/learn_PHP_MVC/addProduct/index") {
+                document.querySelector("#add-link").classList.add("active");
+            } else if (url == "http://localhost/learn_PHP_MVC/update/index") {
+                document.querySelector("#update-link").classList.add("active");
+            } else if (url == "http://localhost/learn_PHP_MVC/delete/index") {
+                document.querySelector("#delete-link").classList.add("active");
+            }
+        })
+    </script>
 </body>
 
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('.menu-toggle').click(function() {
-            $('.menu-toggle').toggleClass('active');
-            $('nav').toggleClass('active');
-            // $('nav ul li a').removeClass('active')   
-        })
-    })
-</script>
+
 
 </html>
